9a40e34e56bc0c997b3ca4ad338f199cbcd08405
xuejiahao <jiahao.xue@nriat.com>
2021-02-03 17:06:37 +0800

    feat: socket Demo创建

---
+++ src/main/java/edu/ives/java/io/bio/ServerSocketDemo.java
@@ -0,0 +1,10 @@
+package edu.ives.java.io.bio;
+
+/**
+ * @version V1.0
+ * @Description: ServerSocketDemo
+ * @author: 薛佳豪
+ * @date: 2021-2-3 17:06
+ */
+public class ServerSocketDemo {
+}
---
+++ src/main/java/edu/ives/java/io/bio/SocketDemo.java
@@ -0,0 +1,10 @@
+package edu.ives.java.io.bio;
+
+/**
+ * @version V1.0
+ * @Description: SocketDemo
+ * @author: 薛佳豪
+ * @date: 2021-2-3 17:05
+ */
+public class SocketDemo {
+}
